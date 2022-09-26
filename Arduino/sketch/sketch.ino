f68d6f4636f9e573b36ad362b62779f0a3e1bff6
iqfareez <iqmal3@outlook.com>
2022-09-26 14:30:13 +0800

    :pencil2: Delete unused & reformat

--- Arduino/sketch/sketch.ino
+++ Arduino/sketch/sketch.ino
@@ -13,27 +13,10 @@ void setup() {
   pinMode(VRY_PIN, INPUT);
 }
 
-// int brightness = 0;    // how bright the LED is
-// int fadeAmount = 5;    // how many points to fade the LED by
-
 void loop() {
-  // if (btSerial.available() > 0) {
-  //   Serial.print(btSerial.readString());
-  // }
-  // change the brightness for next time through the loop:
-  // btSerial.println(brightness);
 
-  // brightness = brightness + fadeAmount;
-
-  // reverse the direction of the fading at the ends of the fade:
-  // if (brightness <= 0 || brightness >= 255) {
-  //   fadeAmount = -fadeAmount;
-  // }
-  // wait for 30 milliseconds to see the dimming effect
-  delay(30);
-    
-  int vrx = analogRead(VRX_PIN);
-  int vry = analogRead(VRY_PIN);
+  int vrx = analogRead(VRX_PIN);  // x-axis joystick
+  int vry = analogRead(VRY_PIN);  // y-axis joystick
 
   String vrxDirection;
   String vryDirection;
@@ -41,26 +24,24 @@ void loop() {
   // technically I can just write if vrx == 1023 (actual exact value)
   // but I didn't want to just to be "safe"
 
-
-
-  if (vrx > 1000) { // exact: 1023
-      vrxDirection = "R";
-  } else if (vrx < 10) { // exact: 0
+  if (vrx > 1000) {  // exact: 1023
+    vrxDirection = "R";
+  } else if (vrx < 10) {  // exact: 0
     vrxDirection = "L";
-  } else { // exact: 505
-    vrxDirection = "C"; // Center
+  } else {               // exact: 505
+    vrxDirection = "C";  // Center
   }
 
-if (vry > 1000) { // exact: 1023
-      vryDirection = "D";
-  } else if (vry < 10) { // exact: 0
+  if (vry > 1000) {  // exact: 1023
+    vryDirection = "D";
+  } else if (vry < 10) {  // exact: 0
     vryDirection = "U";
-  } else { // exact: 505
-    vryDirection = "C"; // Center
+  } else {               // exact: 505
+    vryDirection = "C";  // Center
   }
-  
-  vrxDirection  = "vrx: " + vrxDirection;
-  vryDirection  = "vry: " + vryDirection;
+
+  vrxDirection = "vrx: " + vrxDirection;
+  vryDirection = "vry: " + vryDirection;
 
   Serial.println(vrxDirection);
   Serial.println(vryDirection);
@@ -68,4 +49,5 @@ if (vry > 1000) { // exact: 1023
   btSerial.println(vrxDirection);
   btSerial.println(vryDirection);
 
+  delay(30);
 }
